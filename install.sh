a0ca96c4f0e554c612fc0b6105346627b3054a23
locknic <dom.mortlock@gmail.com>
2019-04-18 06:38:44 +0100

    Add dependencies

--- install.sh
+++ install.sh
@@ -10,4 +10,10 @@ ln -sfv $dir/configs/vimrc ~/.config/nvim/init.vim
 cat $dir/configs/bash_profile > ~/.bash_profile
 source ~/.bash_profile
 
+sudo apt-get update
 sudo apt-get install libssl-dev
+sudo apt-get install default-jdk
+sudo apt-get install gradle
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo apt-get install neovim
